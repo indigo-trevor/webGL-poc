751b8ad8ae094dd5355f1bd82e8cec48ff55b06a
Trevor Robinson <trevor.robinson@indigoslate.com>
2016-08-01 14:41:11 -0700

    opacity

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@
 				for ( var i = 0; i < 1; i ++ ) {
 
 
-					var solo = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { map: texture2 , opacity: 0.65 } ) );
+					var solo = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { map: texture2 , opacity: 1 } ) );
 
 					scene.add( solo );
 
