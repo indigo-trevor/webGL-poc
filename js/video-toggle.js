d07ef2de3cd2ea73fa431c5828681f6f878d142b
Trevor Robinson <trevor.robinson@indigoslate.com>
2016-08-12 10:03:17 -0700

    best one yet

--- js/video-toggle.js
+++ js/video-toggle.js
@@ -1,23 +1,11 @@
 $(document).ready(function(){
-
-
   var videoOne = document.getElementById('video__1');
   var videoTwo = document.getElementById('video__2');
-
   $(videoTwo).hide();
-  
   $( "#toggle-video" ).click(function() {
-
     $(videoOne).toggle();
     $(videoTwo).toggle();
-
-    if ( $(videoTwo).css('display') == 'none' ){
-        videoOne.play();
-    } else {
-        videoTwo.play();
-    }
-
+    videoOne.play();
+    videoTwo.play();
   });
-    
-
 });
