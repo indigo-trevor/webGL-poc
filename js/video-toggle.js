3b4aeefb23b4ffb6045cc5ef1071bd67259f1b19
Trevor Robinson <trevor.robinson@indigoslate.com>
2016-08-12 10:13:33 -0700

    poster in place

--- js/video-toggle.js
+++ js/video-toggle.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var videoOne = document.getElementById('video__1');
   var videoTwo = document.getElementById('video__2');
-  $(videoTwo).hide();
+  $(videoOne).hide();
   $( "#toggle-video" ).click(function() {
     $(videoOne).toggle();
     $(videoTwo).toggle();
